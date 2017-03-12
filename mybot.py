f66ae61d60786a624332883f594cc3d1591ea827
chenxiaoyu <jschenxiaoyu@gmail.com>
2017-03-12 10:32:24 +0800

    change schedule time.

--- mybot.py
+++ mybot.py
@@ -53,7 +53,7 @@ class MyWXBot(WXBot):
                     self.send_file_msg_by_uid(path, uid_test1)
     def schedule(self):
         self.send_msg(u'陈晓宇', 'keepalive')
-        time.sleep(36000)
+        time.sleep(21600)
 
 def main():
     bot = MyWXBot()
